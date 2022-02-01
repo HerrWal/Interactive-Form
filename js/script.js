caa3ede67867b54a9a70b4f04c36aa1bb83edda9
HerrWal <85037202+HerrWal@users.noreply.github.com>
2022-01-31 20:50:02 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log("test");
+
+document.getElementById('name').focus();
+
+
+const otherJob = document.getElementById('other-job-role')
+otherJob.style.display='none';
+
+const jobRole = document.getElementById('title');
+
+jobRole.addEventListener('input', () => {
+    if (event.target.value === 'other') {
+        otherJob.style.display='';
+    } else {
+        otherJob.style.display='none';
+    }
+});
